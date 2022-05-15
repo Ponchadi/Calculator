e00f832e738e0f5a37d4cbb70ad2ca6b06887c8e
Ponchadi <tankkill0@mail.ru>
2022-05-15 18:22:03 +0300

    main

--- main.go
+++ main.go
@@ -11,49 +11,53 @@ func main() {
 	fmt.Println("Calculator in GO language")
 	fmt.Println("Write two numbers:")
 	fmt.Scan(&number1, &number2)
-	fmt.Println("Select an action(\"+\",\"-\",\"*\",\"/\"):")
-	var result int;
+	var result int
 	for i := 1; i < 10; i++ {
+		fmt.Println("Select an action(\"+\",\"-\",\"*\",\"/\",\"=\"):")
 		fmt.Scan(&action)
 		switch action {
 		case "+":
 			if result == 0 {
-				result = Add(number1, number2);
-				fmt.Println("Result: ", result);
-			}else{
-				fmt.Scan(&number2);
-				result = Add(result,number2);
-				fmt.Println("Result: ",result);
+				result = Add(number1, number2)
+				fmt.Println("Result: ", result)
+			} else {
+				fmt.Println("Write the second number")
+				fmt.Scan(&number2)
+				result = Add(result, number2)
+				fmt.Println("Result: ", result)
 			}
 		case "-":
-			if result == 0{
-				result := Sub(number1, number2);
-				fmt.Println("Result: ", result);
-			}else{
-				fmt.Scan(&number2);
-				result = Sub(result,number2);
-				fmt.Println("Result: ",result)
+			if result == 0 {
+				result := Sub(number1, number2)
+				fmt.Println("Result: ", result)
+			} else {
+				fmt.Println("Write the second number")
+				fmt.Scan(&number2)
+				result = Sub(result, number2)
+				fmt.Println("Result: ", result)
 			}
 		case "*":
-			if result == 0{
+			if result == 0 {
 				result := Multiplication(number1, number2)
 				fmt.Println("Result: ", result)
-			}else{
-				fmt.Scan(&number2);
-				result = Multiplication(result,number2);
-				fmt.Println("Result: ",result)
+			} else {
+				fmt.Println("Write the second number")
+				fmt.Scan(&number2)
+				result = Multiplication(result, number2)
+				fmt.Println("Result: ", result)
 			}
 		case "/":
-			if result == 0{
+			if result == 0 {
 				result := Division(number1, number2)
-			   fmt.Println("Result: ", result)
-			}else{
-				fmt.Scan(&number2);
-				result = Division(result,number2);
-				fmt.Println("Result: ",result)
+				fmt.Println("Result: ", result)
+			} else {
+				fmt.Println("Write the second number")
+				fmt.Scan(&number2)
+				result = Division(result, number2)
+				fmt.Println("Result: ", result)
 			}
 		}
-		if (action == "="){
+		if action == "=" {
 			break
 		}
 	}
